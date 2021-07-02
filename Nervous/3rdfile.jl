a5df87411e8745bf77bb1f0970a4a342febbbd42
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2021-07-02 11:50:13 +0300

    week3

--- Nervous/3rdfile.jl
+++ Nervous/3rdfile.jl
@@ -1,3 +1,60 @@
 a, b, c = 1, 2, -3
 
 println(a>b)
+
+a = 1.1
+
+if (a>1)
+    println("a mai mare de 1")
+end
+
+function ifeg1(a)
+    if (a>1)
+        println("a mai mare de 1")
+    end
+end
+
+[ifeg1(x) for x in 0:3]
+
+function ifeg2(a, b)
+    if a>b
+        println("$a mai mare decat $b")
+    else
+        println("$a nu este mai mare decat $b")
+    end
+end
+
+ifeg2(2, 22)
+#################################
+loopvar = 1
+while (loopvar < 4)
+    println("loopvar is $loopvar")
+    global loopvar = loopvar + 1
+end
+
+fibnum = [1, 1]
+while fibnum[end] < 10000
+    push!(fibnum, (fibnum[end-1]+fibnum[end]))
+end
+println(fibnum)
+println(length(fibnum))
+################################
+
+for loopvar in 1:4
+    println(('α' + loopvar)^loopvar)
+end
+
+[ (('α' + loopvar)^loopvar) for loopvar in 1:4]
+
+for loopvar in 1:4
+    println(('α' + loopvar)^loopvar)
+    loopvar = loopvar + 4
+    println("loopvar is $loopvar")
+end
+
+for loopvar in 1:4
+    loopvar = loopvar + 4
+    println("loopvar is $loopvar")    
+    println(('α' + loopvar)^loopvar)
+
+end
