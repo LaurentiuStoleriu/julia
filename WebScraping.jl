23abf70725f60aea17c28868cde25779e7fc1452
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2022-04-24 01:18:48 +0300

    webscraping - first step ok

--- WebScraping.jl
+++ WebScraping.jl
@@ -1,9 +1,19 @@
-using HTTP, Gumbo
+using HTTP, Gumbo, AbstractTrees, JSON
 
-url = "https://publons.com/researcher/1482981/l-stoleriu/publications/"
+url = "https://publons.com/api/profile/publication/1482981/?order_by=date-published&page=1&per_page=30"
 
 r = HTTP.request("GET", url)
 
 r_parsed = parsehtml(String(r.body))
 
-println(r_parsed.root[2][6])
+#println(r_parsed.root[2][6])
+
+#for elem in StatelessBFS(r_parsed.root[2]) println((elem)) end
+
+lista = JSON.parse(r_parsed.root[2][1].text)
+
+#println(lista["results"][i]["journal"]["name"])
+
+for paper in lista["results"]
+    println(paper["journal"]["name"])
+end
