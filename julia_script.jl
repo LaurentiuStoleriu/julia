63a7fa1396df8eed2348c290a3884cc5188981d1
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2022-07-28 16:27:58 +0300

    News from JuliaCon2022

--- julia_script.jl
+++ julia_script.jl
@@ -1,10 +1,29 @@
-c = 1+2
+c = 1 + 2
 
-a=2
+a = 2
 
-function test(a = 1)
-    a+2
+function test(a=1)
+    a + 2
 end
 
 
 test(10)
+
+using DataFrames, RDatasets
+
+iris = dataset("datasets", "iris")
+
+function profile_test(n)
+    for i = 1:n
+        A = randn(100,100,20)
+        m = maximum(A)
+        Am = mapslices(sum, A; dims=2)
+        B = A[:,:,5]
+        Bsort = mapslices(sort, B; dims=1)
+        b = rand(100)
+        C = B.*b
+    end
+end
+
+@profview profile_test(1)
+@profview profile_test(10)
