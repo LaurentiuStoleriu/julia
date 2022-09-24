721c9f503c0540eb4ce14bf35f209ce15ef7dc36
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2022-09-24 14:04:50 +0300

    no need for activate

--- Think/ThinkODE.jl
+++ Think/ThinkODE.jl
@@ -1,6 +1,6 @@
 using DifferentialEquations
 
-α = 10.1
+α = 1.01
 
 f(u,p,t) = α*u
 u0 = 1/2
@@ -9,5 +9,5 @@ prob = ODEProblem(f,u0,tspan)
 sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
 
 using Plots
-plot(sol,linewidth=5,title="Solution to the linear ODE with a thick line", xaxis="Time (t)",yaxis="u(t) (in μm)",label="My Thick Line!") # legend=false
+plot(sol,linewidth=5,title="Solution to the linear ODE with a thick line", xaxis="Time (t)", yaxis="u(t) (in μm)",label="My Thick Line!") # legend=false
 plot!(sol.t, t->0.5*exp(α*t),lw=3,ls=:dash,label="True Solution!")
