ac5bb3ad496ac76c84109b79b826e8f542662c9f
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2022-09-30 21:58:37 +0300

    try aruncare - no result

---
+++ Modelare/_1_aruncare_1.jl
@@ -0,0 +1,17 @@
+using Plots
+
+x0 = 3; y0 = 20; v0 = 10; g = 9.81
+theta = 30.0 * pi/180
+v0x = v0 * cos(theta) 
+v0y = v0 * sin(theta)
+
+function trajectory(t)
+    global x0, y0, v0x, v0y, g
+    return[(x0 + v0x * t), (y0 + v0y*t - g*t*t/2)]
+end
+
+tmax = 2
+numpoints = 100
+
+t = collect(range(start=0, length=numpoints, stop=tmax))
+print(trajectory(t))
