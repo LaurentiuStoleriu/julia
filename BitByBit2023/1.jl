33f73d66407705f55886ddb874ebfb8bb0c10106
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2023-05-20 22:31:04 +0300

    BitByBit1

---
+++ BitByBit2023/1.jl
@@ -0,0 +1,8 @@
+# Given length of side, calculate area of square
+function divide()
+    a::Float64 = pi
+    b::Float64 = 4
+    println("\n π/4 = $(a÷b) \n" )
+end
+
+divide() # call the function to get the action started
