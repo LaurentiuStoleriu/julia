917662d6cb4ad80f27855865279f21d934d65232
Laurentiu Stoleriu <lstoler@staff.uaic.ro>
2024-02-17 12:04:07 +0200

    OKish

--- 1D-potential/1d-julia.jl
+++ 1D-potential/1d-julia.jl
@@ -1,4 +1,4 @@
-using Revise
+#using Revise
 #using BenchmarkTools
 using StaticArrays
 using Plots
@@ -9,7 +9,7 @@ const N = 10
 const rLow = 0.05
 const rHigh = 0.10
 const bondLen = 0.10
-const elasticK = 1.0e0
+const elasticK = 1.0e2
 const A = 1.0e-4
 const factor = pi / (2.0*(rHigh - rLow))
 
@@ -23,12 +23,6 @@ const unuPeTau = 1.0 / tau
 const kBg = kB * g
 const doikB = 2.0 * kB
 
-mutable struct particle
-    x::Float64
-    r::Float64
-    spin::Float64
-end
-
 function probabs(T, p)
 	DkBTpeDoikBT = (D - kBg * T) / (doikB * T)
 	HtoL = unuPeTau * exp(DkBTpeDoikBT) * exp(-(Ea - k * p) / (kB * T))
@@ -36,7 +30,7 @@ function probabs(T, p)
     [HtoL, LtoH]
 end
 
-function createStructSystem!(particles, stare) 
+function createSystem!(x, r, stare) 
     nBumpsPerParticle = 1.0
     currentX = (nBumpsPerParticle * pi / 2.0 / factor)
 	startingRadius = rLow;
@@ -44,10 +38,8 @@ function createStructSystem!(particles, stare)
 		startingRadius = rHigh
     end
 	for i in 1:N
-        #particles[i].x = currentX
-        #particles[i].r = startingRadius
-        #particles[i].spin = stare
-        particles[i] = particle(currentX, startingRadius, stare)
+        x[i] = currentX
+        r[i] = startingRadius
         currentX += (bondLen + 2.0 * startingRadius)
     end
 end
@@ -56,61 +48,92 @@ function surfacePotential(coord::Float64)
     A * (1.0 - sin(factor * coord)) / 2.0
 end
 
-function totalStructEnergy(particles) 
+function totalEnergy(x, r) 
 	VTot = 0.0
     E = 0.0
-	xi = particles[1].x
-	ri = particles[1].r
+	xi = x[1]
+	ri = r[1]
     for i in 1:(N-1)
-		xNext = particles[i + 1].x;
-		rNext = particles[i + 1].r;
-		eBond = -bondLen + xNext - xi - ri - rNext;
-		E += eBond * eBond;
+		xNext = x[i + 1]
+		rNext = r[i + 1]
+		eBond = -bondLen + xNext - xi - ri - rNext
+		E += eBond * eBond
 		VTot += surfacePotential(xi)
 		xi = xNext
 		ri = rNext
         #println("[$i]    E = ", E, "  V = ", VTot)
 	end
-	VTot += surfacePotential(particles[N].x)
+	VTot += surfacePotential(x[N])
     VTot + (elasticK / 2.0) * E
 end
 
-function dTotalStructEnergy!(G, particles) 
-	# VTot = 0.0
-    # E = 0.0
-	# xi = particles[1].x
-	# ri = particles[1].r
-    # for i in 1:(N-1)
-	# 	xNext = particles[i + 1].x;
-	# 	rNext = particles[i + 1].r;
-	# 	eBond = -bondLen + xNext - xi - ri - rNext;
-	# 	E += eBond * eBond;
-	# 	VTot += surfacePotential(xi)
-	# 	xi = xNext
-	# 	ri = rNext
-    #     #println("[$i]    E = ", E, "  V = ", VTot)
-	# end
-	# VTot += surfacePotential(particles[N].x)
-    # VTot + (elasticK / 2.0) * E
+function DtotalEnergy!(G, x, r) 
+	VTot = 0.0
+    E = 0.0
+	xi = x[1]
+	ri = r[1]
+    xNext = x[2]
+    rNext = r[2]
+    VTot = surfacePotential(xi)
+    E = -elasticK * (-bondLen + xNext - xi - ri - rNext)
+    G[1] = VTot + E
+    for i in 2:(N-1)
+        xPrev = xi
+        rPrev = ri
+        xi = xNext
+        ri = rNext
+		xNext = x[i + 1]
+		rNext = r[i + 1]
+        VTot = surfacePotential(xi)
+		E = elasticK * (-xNext - xPrev + 2*xi - rPrev + rNext)
+        G[i] = VTot + E
+	end
+    xPrev = xi
+    rPrev = ri
+    xi = xNext
+    ri = rNext
+    VTot = surfacePotential(xi)
+    E = elasticK * (-bondLen - xPrev + xi - ri - rPrev)
+    G[N] = VTot + E
 end
 
 function main()
-    Revise.revise()
-    particles = Vector{particle}(undef, N)
+    #Revise.revise()
+    #particles = Vector{particle}(undef, N)
 
-    createStructSystem!(particles, 1)
-    totalStructEnergy(particles) 
+    x = @MVector zeros(N)
+    r = @MVector zeros(N)  
+
+    createSystem!(x, r, 0)
+    totalEnergy(x, r) 
 
     ## GRAPHIC ##
-    xGraphic = range(0-3*bondLen, particles[N].x+3*bondLen, length=1000)
+    xGraphic = range(0-3*bondLen, x[N]+3*bondLen, length=1000)
     plot(xGraphic, map(surfacePotential, xGraphic))
-    xCoords = [particles[i].x for i in 1:N]
+    xCoords = x[1:N]
     scatter!(xCoords, map(surfacePotential, xCoords))
     plot!(legend=:outerbottom, legendcolumns=2)
 
-    results = optimize(f, g!, x_iv, LBFGS()) # or ConjugateGradient()
-    println("minimum = $(results.minimum) with argmin = $(results.minimizer) in "*"$(results.iterations) iterations")
+    r[5] = rHigh
+
+    println(x)
+    println(r)
+
+    f(x) = totalEnergy(x, r)
+    g!(G, x) = DtotalEnergy!(G, x, r)
+
+    results = optimize(f, x, LBFGS()) # or ConjugateGradient()
+    #println("minimum = $(results.minimum) with argmin = $(results.minimizer) in "*"$(results.iterations) iterations")
     
+    xNew = Optim.minimizer(results)[1:N]
+    println(Optim.minimizer(results), "  ", Optim.converged(results))
+    println(r)
+
+    xGraphic = range(0-3*bondLen, xNew[N]+3*bondLen, length=1000)
+    plot(xGraphic, map(surfacePotential, xGraphic))
+    xCoords = xNew[1:N]
+    scatter!(xCoords, map(surfacePotential, xCoords))
+    plot!(legend=:outerbottom, legendcolumns=2)
 end
 
 main()
